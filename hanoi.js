fe177618471303e0d14c625b79887a9d34841a47
lovelytong <tongyu@tongyudeMacBook-Air.local>
2018-04-12 14:01:18 +0800

    finish

--- hanoi.js
+++ hanoi.js
@@ -1,4 +1,4 @@
-var floor = 2
+var floor = 1
 async function hanoi(a,b,c,count) {
   if (count > 0) {
      await hanoi(a,c,b,count-1);
@@ -7,21 +7,37 @@ async function hanoi(a,b,c,count) {
   }
 }
 
-async function move(a,b) {
-  return new Promise((resolve) => {
+function move(a,b) {
+  return new Promise((resolve,reject) => {
     setTimeout(()=>{
-      x = $('#'+ a).children(":first");
+      x = $('#'+ a).children(":first").remove();
       $('#'+ b).prepend(x);
+      $('#board').text(a + '=>' + b)
       resolve();
     },1000);
   })
 }
 
-$(document).ready(function () {
-  $('#one').addClass("one");
+async function button_ready(){
+  $("button").removeAttr("disabled");
+}
+
+function play() {
+  $('button').attr("disabled","disabled");
+  $('#c').empty()
+  floor = $('input').val()
+  $('#a').prepend('<div id="one" class="one"></div>');
   for (var i = 0; i < floor-1; i++) {
     $('#one').after('<div class="one"></div>');
     $('#one').next().css('width',(50 + (floor - 1 - i) * 20).toString() + 'px');
   }
-  hanoi('a', 'b', 'c', floor)
+  hanoi('a', 'b', 'c', floor).then(button_ready);
+
+}
+
+$(document).ready(function () {
+  $('button').click(play)
 })
+
+
+
